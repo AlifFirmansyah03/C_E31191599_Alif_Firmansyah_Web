5116da1421e3e38136a933d2840b46c50ec54726
AlifFirmansyah03 <aliffirmansyah1804@gmail.com>
2021-03-15 23:56:18 +0700

    E3119599

---
+++ Minggu6/application/models/Hello_model.php
@@ -0,0 +1,12 @@
+<?php
+class Hello_model extends CI_Model{
+    //public $txt = 'Hello World!';
+    
+    public $alert = 'Hello Wolrd dari CI Model';
+    
+    public $mahasiswa = [
+			["nama"=>"Alif","nim"=>"E31191599","gol"=>"C"],
+			["nama"=>"Firmansyah","nim"=>"E31191515","gol"=>"C"],
+		];
+}
+?>

f70081c39384de6403f69a9c7589e7201d32ea55
AlifFirmansyah03 <aliffirmansyah1804@gmail.com>
2021-04-06 09:43:06 +0700

    Tugas Minggu 7
    Tugas Minggu 7

---
+++ Minggu7/application/models/Sno4_model.php
@@ -0,0 +1,8 @@
+<?php
+class Sno4_model extends CI_Model {
+  public $nim = 'E31191599';
+  public $nama = 'Alif Firmansyah';
+  public $jurusan = 'TI';
+  public $prodi = 'MIF';
+  public $kotaAsal = 'Jember';
+}
